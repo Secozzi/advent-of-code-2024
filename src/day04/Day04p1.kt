a78fbdbaebc3e37275d709273bd8d9c07a12abd7
Secozzi <folke.steen85@gmail.com>
2024-12-04 20:03:39 +0100

    Day 4 solutions

--- src/day04/Day04p1.kt
+++ src/day04/Day04p1.kt
@@ -4,16 +4,44 @@ import readInput
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    fun wordCount(grid: List<CharArray>, word: String): Int {
+        val directions = listOf(
+            Pair(-1, -1), Pair(0, -1), Pair( 1, -1),
+            Pair(-1,  0),              Pair( 1,  0),
+            Pair(-1,  1), Pair(0,  1), Pair( 1,  1),
+        )
+
+        fun isWordInDirection(x: Int, y: Int, dx: Int, dy: Int): Boolean {
+            word.forEachIndexed { index, ch ->
+                val newX = x + dx * index
+                val newY = y + dy * index
+
+                if (newX >= grid.first().size || newX < 0) return false
+                if (newY >= grid.size || newY < 0) return false
+                if (grid[newY][newX] != ch) return false
+            }
+
+            return true
+        }
+
+        return grid.first().indices.sumOf { x ->
+            grid.indices.sumOf { y ->
+                directions.count { (dx, dy) -> isWordInDirection(x, y, dx, dy) }
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val grid = input.map(String::toCharArray)
+
+        return wordCount(grid, "XMAS")
     }
 
     // Test
     val test = readInput(4, isTest = true)
     println("test=${part1(test)}")
-    return
 
-    // Final solution
+    // Final solution - 2593
     val input = readInput(4)
     val time = measureTimeMillis {
         println("answer=${part1(input)}")
--- src/day04/Day04p2.kt
+++ src/day04/Day04p2.kt
@@ -4,14 +4,26 @@ import readInput
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    fun IntRange.dropLast(n: Int): IntRange {
+        return IntRange(start = start, endInclusive = endInclusive - n)
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val grid = input.map(String::toCharArray)
+
+        return grid.first().indices.dropLast(2).sumOf { x ->
+            grid.indices.dropLast(2).count { y ->
+                val firstDiagonal = (grid[y][x] == 'M' && grid[y + 2][x + 2] == 'S') || (grid[y][x] == 'S' && grid[y + 2][x + 2] == 'M')
+                val secondDiagonal = (grid[y + 2][x] == 'M' && grid[y][x + 2] == 'S') || (grid[y + 2][x] == 'S' && grid[y][x + 2] == 'M')
+
+                grid[y + 1][x + 1] == 'A' && firstDiagonal && secondDiagonal
+            }
+        }
     }
 
     // Test
     val test = readInput(4, isTest = true)
     println("test=${part2(test)}")
-    return
 
     // Final solution
     val input = readInput(4)
