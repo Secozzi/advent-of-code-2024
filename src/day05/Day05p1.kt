6d6a855e04f143f09b07c6eef7ec20385bd42c7d
Secozzi <folke.steen85@gmail.com>
2024-12-05 07:54:58 +0100

    Day 5 solutions

--- src/day05/Day05p1.kt
+++ src/day05/Day05p1.kt
@@ -3,15 +3,38 @@ package day05
 import readInput
 import kotlin.system.measureTimeMillis
 
+fun List<Int>.isCorrect(rules: List<Pair<Int, Int>>): Boolean {
+    rules.forEach { (before, after) ->
+        val beforeIndex = this.indexOf(before)
+        val afterIndex = this.indexOf(after)
+
+        if (beforeIndex == -1 || afterIndex == -1) return@forEach
+        if (beforeIndex > afterIndex) return false
+    }
+
+    return true
+}
+
+fun List<Int>.middle(): Int {
+    return this[this.size / 2]
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (rulesTxt, pagesTxt) = input.joinToString("\n").split("\n\n")
+        val rules = rulesTxt.lines().map { it.split("|").let { (a, b) -> a.toInt() to b.toInt() } }
+        val pages = pagesTxt.lines().map { it.split(",").map(String::toInt) }
+
+        return pages
+            .filter { it.isCorrect(rules) }
+            .sumOf { page ->
+                page.middle()
+            }
     }
 
     // Test
     val test = readInput(5, isTest = true)
     println("test=${part1(test)}")
-    return
 
     // Final solution
     val input = readInput(5)
--- src/day05/Day05p2.kt
+++ src/day05/Day05p2.kt
@@ -4,14 +4,41 @@ import readInput
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    fun List<Int>.fix(rules: List<Pair<Int, Int>>): List<Int> {
+        val result = toMutableList()
+
+        rules.forEach { (before, after) ->
+            val beforeIndex = result.indexOf(before)
+            val afterIndex = result.indexOf(after)
+
+            if (beforeIndex == -1 || afterIndex == -1) return@forEach
+            if (beforeIndex < afterIndex) return@forEach
+
+            result[beforeIndex] = result[afterIndex].also { result[afterIndex] = result[beforeIndex] }
+        }
+
+        return result
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val (rulesTxt, pagesTxt) = input.joinToString("\n").split("\n\n")
+        val rules = rulesTxt.lines().map { it.split("|").let { (a, b) -> a.toInt() to b.toInt() } }
+        val pages = pagesTxt.lines().map { it.split(",").map(String::toInt) }
+
+        return pages
+            .filterNot { it.isCorrect(rules) }
+            .sumOf { page ->
+                var fixed = page
+                while (!fixed.isCorrect(rules)) {
+                    fixed = fixed.fix(rules)
+                }
+                fixed.middle()
+            }
     }
 
     // Test
     val test = readInput(5, isTest = true)
     println("test=${part2(test)}")
-    return
 
     // Final solution
     val input = readInput(5)
