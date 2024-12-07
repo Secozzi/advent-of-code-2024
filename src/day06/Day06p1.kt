e5620bed5c6f9d61d30c8c35b67bae4d511e31bd
Secozzi <folke.steen85@gmail.com>
2024-12-07 07:39:25 +0100

    Day 6 solutions

--- src/day06/Day06p1.kt
+++ src/day06/Day06p1.kt
@@ -3,15 +3,75 @@ package day06
 import readInput
 import kotlin.system.measureTimeMillis
 
+enum class Direction(val dx: Int, val dy: Int) {
+    Up(0, 1),
+    Right(1, 0),
+    Down(0, -1),
+    Left(-1, 0);
+
+    fun getNext(): Direction {
+        return entries[(ordinal + 1) % entries.size]
+    }
+}
+
+fun isOnEdge(grid: List<List<Char>>, x: Int, y: Int, direction: Direction): Boolean {
+    return when (direction) {
+        Direction.Up -> y == 0
+        Direction.Right -> x == grid.first().size - 1
+        Direction.Down -> y == grid.size - 1
+        Direction.Left -> x == 0
+    }
+}
+
+fun isNextObstacle(grid: List<List<Char>>, x: Int, y: Int, direction: Direction): Boolean {
+    val newX = x + direction.dx
+    val newY = y - direction.dy
+
+    return grid[newY][newX] == '#'
+}
+
+fun getPositions(grid: List<List<Char>>, x: Int, y: Int, dir: Direction): List<Pair<Int, Int>> {
+    val visited = mutableSetOf<Pair<Int, Int>>()
+
+    var currentX = x
+    var currentY = y
+    var direction = dir
+
+    while (true) {
+        visited.add(currentX to currentY)
+
+        if (isNextObstacle(grid, currentX, currentY, direction)) {
+            direction = direction.getNext()
+        } else {
+            currentX += direction.dx
+            currentY -= direction.dy
+        }
+
+        if (isOnEdge(grid, currentX, currentY, direction)) {
+            visited.add(currentX to currentY)
+            return visited.toList()
+        }
+    }
+}
+
+fun getStartPos(grid: List<List<Char>>): Pair<Int, Int> {
+    val startRow = grid.first { chars -> chars.contains('^') }
+    return Pair(startRow.indexOf('^'), grid.indexOf(startRow))
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val grid = input.map(String::toList)
+
+        val (startX, startY) = getStartPos(grid)
+        val direction = Direction.Up
+
+        return getPositions(grid, startX, startY, direction).size
     }
 
     // Test
     val test = readInput(6, isTest = true)
     println("test=${part1(test)}")
-    return
 
     // Final solution
     val input = readInput(6)
--- src/day06/Day06p2.kt
+++ src/day06/Day06p2.kt
@@ -4,14 +4,59 @@ import readInput
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    fun hasLoop(grid: List<List<Char>>, x: Int, y: Int, dir: Direction): Boolean {
+        var currentX = x
+        var currentY = y
+        var direction = dir
+        val visited = mutableListOf<Triple<Int, Int, Direction>>()
+
+        while (true) {
+            visited.add(Triple(currentX, currentY, direction))
+
+            if (isNextObstacle(grid, currentX, currentY, direction)) {
+                direction = direction.getNext()
+            } else {
+                currentX += direction.dx
+                currentY -= direction.dy
+            }
+
+            if (visited.contains(Triple(currentX, currentY, direction))) {
+                return true
+            }
+
+            if (isOnEdge(grid, currentX, currentY, direction)) {
+                return false
+            }
+        }
+    }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val grid = input.map(String::toMutableList)
+
+        val (startX, startY) = getStartPos(grid)
+        val direction = Direction.Up
+
+        val visitedList = getPositions(grid, startX, startY, direction) - Pair(startX, startY)
+
+        var count = 0
+        visitedList.forEachIndexed { index, (x, y) ->
+            grid[y][x] = '#'
+            if (hasLoop(grid, startX, startY, direction)) {
+                count++
+            }
+            grid[y][x] = '.'
+
+            val progress = (index + 1) * 100f / visitedList.size
+            print("\r Progress: %.2f%%".format(progress))
+        }
+        println()
+
+        return count
     }
 
     // Test
     val test = readInput(6, isTest = true)
     println("test=${part2(test)}")
-    return
 
     // Final solution
     val input = readInput(6)
