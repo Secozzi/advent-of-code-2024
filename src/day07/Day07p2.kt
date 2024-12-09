6e895c15000fcd933aa9ec63837cf75e815d8551
Secozzi <folke.steen85@gmail.com>
2024-12-09 15:51:04 +0100

    Day 7 solutions

--- src/day07/Day07p2.kt
+++ src/day07/Day07p2.kt
@@ -4,14 +4,31 @@ import readInput
 import kotlin.system.measureTimeMillis
 
 fun main() {
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun isValid(
+        testValue: Long,
+        values: List<Long>,
+    ): Boolean {
+        if (values.size == 1) {
+            return testValue == values.first()
+        }
+
+        return isValid(testValue, listOf(values[0] + values[1]) + values.drop(2)) ||
+                isValid(testValue, listOf(values[0] * values[1]) + values.drop(2)) ||
+                isValid(testValue, listOf((values[0].toString() + values[1].toString()).toLong()) + values.drop(2))
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.sumOf { line ->
+            val (testValue, values) = line.split(": ").let { strs ->
+                strs[0].toLong() to strs[1].split(" ").map(String::toLong)
+            }
+            if (isValid(testValue, values)) testValue else 0L
+        }
     }
 
     // Test
     val test = readInput(7, isTest = true)
     println("test=${part2(test)}")
-    return
 
     // Final solution
     val input = readInput(7)
