4f56e6caca90ef5b69cef4e36c9ebda73dfa7ac7
Secozzi <folke.steen85@gmail.com>
2024-12-13 06:35:02 +0100

    Day 9 solutions

--- src/day09/Day09p1.kt
+++ src/day09/Day09p1.kt
@@ -3,15 +3,52 @@ package day09
 import readInput
 import kotlin.system.measureTimeMillis
 
+sealed class Block {
+    data object Space : Block()
+    data class File(val id: Long) : Block()
+}
+
+fun List<Block>.checkSum(): Long {
+    return this.mapIndexed { index, block ->
+        if (block is Block.File) index * block.id else 0L
+    }.sum()
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parse(diskMap: String): List<Block> {
+        return "${diskMap}0".toList().chunked(2).flatMapIndexed { idx, (file, space) ->
+            List(file.digitToInt()) { Block.File(idx.toLong()) } + List(space.digitToInt()) { Block.Space }
+        }
+    }
+
+    fun List<Block>.getFirstSpaceIndex(): Int {
+        return this.indexOfFirst { it is Block.Space }
+    }
+
+    fun List<Block>.getLastFileIndex(): Int {
+        return this.indexOfLast { it is Block.File }
+    }
+
+    fun isFilled(blockList: List<Block>): Boolean {
+        return blockList.getFirstSpaceIndex() > blockList.getLastFileIndex()
+    }
+
+    fun part1(input: List<String>): Long {
+        val blockList = parse(input.first()).toMutableList()
+
+        while (!isFilled(blockList)) {
+            val firstSpace = blockList.getFirstSpaceIndex()
+            val lastFile = blockList.getLastFileIndex()
+
+            blockList[firstSpace] = blockList[lastFile].also { blockList[lastFile] = blockList[firstSpace] }
+        }
+
+        return blockList.checkSum()
     }
 
     // Test
     val test = readInput(9, isTest = true)
     println("test=${part1(test)}")
-    return
 
     // Final solution
     val input = readInput(9)
--- src/day09/Day09p2.kt
+++ src/day09/Day09p2.kt
@@ -3,15 +3,65 @@ package day09
 import readInput
 import kotlin.system.measureTimeMillis
 
+sealed class SizedBlock(val length: Int) {
+    class Space(length: Int) : SizedBlock(length)
+    class File(length: Int, val id: Long) : SizedBlock(length)
+}
+
 fun main() {
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun parse(diskMap: String): List<SizedBlock> {
+        return "${diskMap}0".toList().chunked(2).flatMapIndexed { idx, (file, space) ->
+            listOf(
+                SizedBlock.File(length = file.digitToInt(), id = idx.toLong()),
+                SizedBlock.Space(length = space.digitToInt()),
+            )
+        }
+    }
+
+    fun List<SizedBlock>.getIndexFromId(id: Long): Int {
+        return this.indexOfFirst { it is SizedBlock.File && it.id == id }
+    }
+
+    fun List<SizedBlock>.getFirstAvailableSpaceIndex(size: Int): Int {
+        return this.indexOfFirst { it is SizedBlock.Space && it.length >= size }
+    }
+
+    fun MutableList<SizedBlock>.swapFileAndSpace(start: Int, end: Int) {
+        val spaceSize = this[start].length
+        val fileSize = this[end].length
+
+        this[start] = this[end].also { this[end] = SizedBlock.Space(length = fileSize) }
+
+        if (spaceSize > fileSize) {
+            add(start + 1, SizedBlock.Space(length = spaceSize - fileSize))
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        val sizedBlockList = parse(input.first()).toMutableList()
+
+        (sizedBlockList.count { it is SizedBlock.File } - 1 downTo 0).forEach { id ->
+            val lastFile = sizedBlockList.getIndexFromId(id.toLong())
+            val firstSpace = sizedBlockList.getFirstAvailableSpaceIndex(sizedBlockList[lastFile].length)
+
+            if (firstSpace > 0 && firstSpace < lastFile) {
+                sizedBlockList.swapFileAndSpace(firstSpace, lastFile)
+            }
+        }
+
+        val blockList = sizedBlockList.flatMap { sizedBlock ->
+            when (sizedBlock) {
+                is SizedBlock.File -> List(sizedBlock.length) { Block.File(id = sizedBlock.id) }
+                is SizedBlock.Space -> List(sizedBlock.length) { Block.Space }
+            }
+        }
+
+        return blockList.checkSum()
     }
 
     // Test
     val test = readInput(9, isTest = true)
     println("test=${part2(test)}")
-    return
 
     // Final solution
     val input = readInput(9)
